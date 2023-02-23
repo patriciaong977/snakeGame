a8e792a7832f8f2937fed2c425cf4e8cc7eb7671
patriciaong977 <patricia.ong977@gmail.com>
2023-02-22 23:55:29 -0600

    Updated movement and added snake class.

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ screen.tracer(0)  # Turn off the screen updates.
 
 snake = Snake()
 
+# Move the snake with the arrow keys.
+screen.listen()
+screen.onkey(snake.m_up, "Up")
+screen.onkey(snake.m_down, "Down")
+screen.onkey(snake.m_left, "Left")
+screen.onkey(snake.m_right, "Right")
+
 GAME_IS_ON = True
 while GAME_IS_ON:
     screen.update()
--- snake.py
+++ snake.py
@@ -1,6 +1,10 @@
 from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     '''Creating the Snake body, and Moving the Snake'''
@@ -8,6 +12,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         '''S1: Create the snake body. Turtle = 20x20 pixels'''
@@ -25,4 +30,28 @@ class Snake:
             # new_x = self.segments[seg_num - 1].xcor()
             # new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(self.segments[seg_num - 1].pos())
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def m_up(self):
+        ''' Move the snake up. Bearing = 90 degrees
+        If the snake is moving down, it can't move up.'''
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def m_down(self):
+        ''' Move the snake down. Bearing = 270 degrees
+        If the snake is moving up, it can't move down.'''
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def m_left(self):
+        ''' Move the snake left. Bearing = 180 degrees
+        If the snake is moving right, it can't move left.'''
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def m_right(self):
+        ''' Move the snake right. Bearing = 0 degrees
+        If the snake is moving left, it can't move right.'''
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
